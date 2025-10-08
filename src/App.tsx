032bf11008109e7a8598fa457761d86b08e646bf
unknown <you@example.comlagg312@gmail.com>
2025-10-08 19:45:10 -0300

    Deploy GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,47 @@ import ClientDetail from "./pages/ClientDetail";
 import Login from "./auth/Login";
 import AdminUsers from "./pages/AdminUsers";
 import Layout from "./components/Layout";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function App(){
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Navigate to="/clientes" replace/>} />
-        <Route path="/login" element={<Login/>}/>
-        <Route
-          path="/clientes"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <ClientsList />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/clientes/:id"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <ClientDetail />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/usuarios"
-          element={
-            <ProtectedRoute allowedRoles={["admin"]}>
-              <Layout>
-                <AdminUsers />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/clientes" replace/>} />
+          <Route path="/login" element={<Login/>}/>
+          <Route
+            path="/clientes"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <ClientsList />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/clientes/:id"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <ClientDetail />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/usuarios"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <Layout>
+                  <AdminUsers />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </HashRouter>
     </AuthProvider>
   );
 }
