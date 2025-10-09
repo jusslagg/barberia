37f7b325262bf54169f1c9fb6bc2c0a243523b13
unknown <you@example.comlagg312@gmail.com>
2025-10-09 01:28:30 -0300

    nuevo dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/barberia/", 
+  base: "/barberia/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
